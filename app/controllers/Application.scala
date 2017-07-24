f962a772c5119e64e8cda493beff7a44e6163cf1
ericohwotu <eric.ohwotu@outlook.com>
2017-07-24 15:06:05 +0000

    Added Client Page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,27 @@ import play.api.mvc._
 
 class Application extends Controller {
 
+  val num = 488
+
+  val ok = Ok("Hello World")
+  val notFound = NotFound
+  val pageNotFound = NotFound("<h1>Page Not Found</h1>")
+  val badRequest = BadRequest("Error Happened")
+  val oops = InternalServerError("Oops")
+  val anyStatus = Status(num)("strange response type")
+
+
   def index = Action {
-    Ok("i am ready")
+    ok
   }
 
+  def redirect = Action {
+      Redirect("http://www.Facebook.com")
+  }
+
+  def reverse = Action { implicit request: Request[AnyContent] =>
+    Redirect(routes.Application.index())
+  }
+
+  def notImplemented = TODO
 }
