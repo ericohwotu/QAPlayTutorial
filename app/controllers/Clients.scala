2be2795c66629f0a3fe81a79362d1e0023418d81
ericohwotu <eric.ohwotu@outlook.com>
2017-07-24 15:25:42 +0000

    Added Client Page

--- app/controllers/Clients.scala
+++ app/controllers/Clients.scala
@@ -1,5 +1,25 @@
 package controllers
 
-class Clients {
+import play.api.mvc._
 
+import play.api._
+
+class Clients extends Controller{
+
+
+  def list = Action {
+    Ok("show clients")
+  }
+
+  def greet(name: String) = Action {
+    Ok(s"hello $name")
+  }
+
+  def show(id: Long) = Action {
+    Ok(s"you typed in that you wanted to view <strong>$id<strong>")
+  }
+
+  def showString(id: String) = Action {
+    Ok(s"same as before $id")
+  }
 }
